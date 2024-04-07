0e1a5b02c3c821db930d6d43aa3455e1c380e798
Lucas Domingues <125484200+lucasdominguess@users.noreply.github.com>
2024-04-07 13:59:21 -0300

    modify css

--- views/index.php
+++ views/index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.datatables.net/v/bs5/dt-2.0.2/af-2.7.0/b-3.0.1/b-html5-3.0.1/fc-5.0.0/fh-4.0.1/r-3.0.0/rg-1.5.0/rr-1.5.0/sb-1.7.0/sl-2.0.0/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="index.css">
+    <!-- <link rel="stylesheet" href="index.css"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Poppins:wght@100;400&display=swap" rel="stylesheet">
@@ -19,19 +19,20 @@
 
 <body class="body_bg">
         
-    <!-- Sessao cadastro -->
-    <section id="sessao_login" class="">
-            <!-- <div id="div-img"> <img id="img" class="opacity-50" src="img.svg" alt="">
-            </div> -->
-            <div class="" id="login">
+    <!-- Sessao login -->
+    <section id="sessao" class="container-fluid">
+
+            <div id="div-img"> <img id="img" class="opacity-50" src="assets/img/img2.svg" alt="">
+            </div>
+            <div id="login">
 
             <form id="form_login">
-                <div class="modal-content">
+                <div class="div_form">
                     <div>
             
                         <div class="conteudo">
    
-                            <div id="h3login">
+                            <div id="div_h3login">
                                 <h3 id="title_login">Login</h3> 
                             </div> 
                             <input type="hidden" name="id" id="id">
@@ -51,14 +52,12 @@
         </section>
 </body>
 
-
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.0.2/af-2.7.0/b-3.0.1/b-colvis-3.0.1/b-html5-3.0.1/fc-5.0.0/fh-4.0.1/r-3.0.0/rg-1.5.0/rr-1.5.0/sb-1.7.0/sl-2.0.0/datatables.min.js"></script>
 
-<script src=""></script>
 <script src="assets/js/listar_dados.js"></script>
 <script src="assets/js/editar_dados.js"></script>
 <script src="assets/js/enviar_dados.js"></script>
--- views/registrar.php
+++ views/registrar.php
@@ -7,9 +7,9 @@
 
 // $nome = $_SESSION['nome'] ?? '';
 // $logado = $_SESSION['sessao'] ?? 'ffff';
-// $logado2= $_SESSION['sessao']->format('H:i:s');
-// $tempo = $_SESSION['tempo30'] ?? 'vazio' ;
-// $newtime = $tempo->format("H:i:s");
+// $logado2 = $_SESSION['sessao']->format('H:i:s');
+// // $tempo = $_SESSION['tempo30'] ?? 'vazio' ;
+// // $newtime = $tempo->format("H:i:s");
 
 
 
@@ -25,72 +25,74 @@
     <link href="https://cdn.datatables.net/v/bs5/dt-2.0.2/af-2.7.0/b-3.0.1/b-html5-3.0.1/fc-5.0.0/fh-4.0.1/r-3.0.0/rg-1.5.0/rr-1.5.0/sb-1.7.0/sl-2.0.0/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/registrar.css">
+    <link rel="stylesheet" href="css/registrar.css">
     <title>Cadastro</title>
 </head>
 
 <body class="body_bg">
 
     <section id="conteudo">
-        
-    <header id="header">
-        
-        <section id="sessao_adm" class="col-2">
+
+        <header id="header">
+
+            <section id="sessao_adm" class="col-2">
                 <div id="nomeAdm">
                     <div>
                         <h4>Bem-vindo</h4>
-                       <h3> </h3> 
+                        <h3><?php echo $nome; ?> ! </h3>
                     </div>
                 </div>
                 <div id="tempo_sessao">
-                    <?php    ?> <br> 
+                    <?php echo $logado2;   ?> <br>
                 </div>
                 <div>
                     <button class="btn btn-danger">Sair</button>
                 </div>
-                    
-        </section>
-                
-        <section id="nav" class='col-sm-8'>
+
+            </section>
+
+            <section id="nav" class='col-sm-8'>
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-            </a>
-            <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-            </ul>
-            <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            </div>
-            </div>
-            </nav>
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                </li>
+                            </ul>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
                 </div>
-             
-        </section>
+
+            </section>
         </header>
         <!-- Sessao cadastro -->
         <section id="sessao_cadastro">
@@ -100,10 +102,10 @@
         <section id="sessao_tabela" class="container col-sm-8">
 
             <div id="div_btncad">
-            <button id="btn_cadastrar" class="btn btn-primary" type="button">Cadastrar</button>
+                <button id="btn_cadastrar" class="btn btn-primary" type="button">Cadastrar</button>
 
             </div>
-            
+
         </section>
 
     </section>
@@ -111,43 +113,43 @@
     <!-- Sessao modal  -->
     <section id="sessao_modal">
 
-            <div class="modal bg-transp" id="myModal">
-                <div class="modal-dialog">
-                    <div class="modal-content">
+        <div class="modal bg-transp" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
 
-                   
-                        <div class="modal-header">
-                            <h4 class="modal-title">Insira os novos dados de edição</h4>
-                            <button id="btn_close" type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                        </div>
 
-                
-                        <div class="modal-body">
-                          
-                                    <form id="form_cad">
-                                    
-                        
-                                                    <h3 id="title_h3">Cadastro</h3>
-                                                    <input type="hidden" name="id" id="id">
-                                                    <label for="nome"><b>Nome:</b></label><br>
-                                                    <input type="text" id="nome" name="nome" placeholder="Digite seu nome aqui!"><br><br>
-                                                    <label for="data_nascimento"><b>Data de Nascimento:</b></label><br>
-                                                    <input type="date" id="data" name="data_nascimento"><br><br>
-                                                   
-                                        
-                                    </form>
-                        </div>
+                    <div class="modal-header">
+                        <!-- <h4 class="modal-title">Insira os novos dados de edição</h4> -->
+                        <button id="btn_close" type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
 
-                        <div class="modal-footer">
-                            <button id="fechar" type="button" class="btn btn-danger">Fechar</button>
-                            <button id="btn_enviarCad" type="button" class="btn btn-success">Enviar</button>
-                        </div>
+
+                    <div class="modal-body">
+
+                        <form id="form_cad" class="">
+
+
+                            <h3 id="title_h3">Cadastro</h3>
+                            <input type="hidden" name="id" id="id"  class="form-control">
+                            <label for="nome" class="form-label"><b>Nome:</b></label><br>
+                            <input type="text" id="nome" name="nome" placeholder="Digite seu nome aqui!"  class="form-control"><br><br>
+                            <label for="data_nascimento" class="form-label"><b>Data de Nascimento:</b></label><br>
+                            <input type="date" id="data" name="data_nascimento"  class="form-control"><br><br>
+
+
+                        </form>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button id="fechar" type="button" class="btn btn-danger">Fechar</button>
+                        <button id="btn_enviarCad" type="button" class="btn btn-success">Enviar</button>
                     </div>
                 </div>
             </div>
-            
+        </div>
 
-        </section>
+
+    </section>
 
 
 </body>
@@ -159,10 +161,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.0.2/af-2.7.0/b-3.0.1/b-colvis-3.0.1/b-html5-3.0.1/fc-5.0.0/fh-4.0.1/r-3.0.0/rg-1.5.0/rr-1.5.0/sb-1.7.0/sl-2.0.0/datatables.min.js"></script>
 
-<script src="assets/js/listar_dados.js"></script>
-<script src="assets/js/editar_dados.js"></script>
-<script src="assets/js/enviar_dados.js"></script>
-<script src="assets/js/Excluir_dados.js"></script>
-<script src="assets/js/index.js"></script>
+<script src="listar_dados.js"></script>
+<script src="editar_dados.js"></script>
+<script src="enviar_dados.js"></script>
+<script src="Excluir_dados.js"></script>
+<script src="verifica_token.js"></script>
+<script src="index.js"></script>
 
 </html>
